a4c5994c0ebb47060a9f6f1f678b91e1c2961fa4
Wlloiam <118074610+Wlloiam@users.noreply.github.com>
2024-04-27 00:17:58 -0700

    lab-11-WilliamTong

--- Lab-11-Float/firmware/src/asmFmax.s
+++ Lab-11-Float/firmware/src/asmFmax.s
@@ -65,7 +65,45 @@ nanValue: .word 0x7FFFFFFF
  .type initVariables,%function
 initVariables:
     /* YOUR initVariables CODE BELOW THIS LINE! Don't forget to push and pop! */
-
+    push {r4-r11,LR}
+    
+    mov r10,0
+    mov r11,1
+    ldr r4,=f1
+    str r0,[r4]
+    ldr r4,=sb1
+    str r10,[r4]
+    ldr r4,=biasedExp1
+    str r10,[r4]
+    ldr r4,=exp1
+    str r10,[r4]
+    ldr r4,=mant1
+    str r10,[r4]
+    
+    ldr r4,=f2
+    str r1,[r4]
+    ldr r4,=sb2
+    str r10,[r4]
+    ldr r4,=biasedExp2
+    str r10,[r4]
+    ldr r4,=exp2
+    str r10,[r4]
+    ldr r4,=mant2
+    str r10,[r4]
+    
+    ldr r4,=fMax
+    str r10,[r4]
+    ldr r4,=signBitMax
+    str r10,[r4]
+    ldr r4,=biasedExpMax
+    str r10,[r4]
+    ldr r4,=expMax
+    str r10,[r4]
+    ldr r4,=mantMax
+    str r10,[r4]
+    
+    pop {r4-r11,LR}
+    mov pc, lr
     /* YOUR initVariables CODE ABOVE THIS LINE! Don't forget to push and pop! */
 
     
@@ -82,7 +120,18 @@ initVariables:
 .type getSignBit,%function
 getSignBit:
     /* YOUR getSignBit CODE BELOW THIS LINE! Don't forget to push and pop! */
-
+    push {r4-r11,LR}
+    mov r11,1
+    mov r10,0
+    mov r4,0x80000000
+    ldr r5,[r0]
+    ands r5,r5,r4
+    movmi r5,r11
+    movpl r5,r10
+    str r5,[r1]
+    
+    pop {r4-r11,LR}
+    mov pc, lr
     /* YOUR getSignBit CODE ABOVE THIS LINE! Don't forget to push and pop! */
     
 
@@ -109,7 +158,20 @@ getSignBit:
 .type getExponent,%function
 getExponent:
     /* YOUR getExponent CODE BELOW THIS LINE! Don't forget to push and pop! */
+    push {r4-r11,LR}
     
+    ldr r4,=0x7F800000
+    ldr r5,[r0]
+    AND r5,r5,r4
+    lsr r5,r5,23
+    
+    str r5,[r1]
+    sub r5,r5,127
+    str r5,[r2]
+    
+    
+    pop {r4-r11,LR}
+    mov pc, lr
     /* YOUR getExponent CODE ABOVE THIS LINE! Don't forget to push and pop! */
    
 
@@ -127,7 +189,40 @@ getExponent:
 .type getMantissa,%function
 getMantissa:
     /* YOUR getMantissa CODE BELOW THIS LINE! Don't forget to push and pop! */
+    push {r4-r11,LR}
     
+    ldr r6,=0x7FFFFF
+    ldr r7,=0x7F800000 
+    ldr r8,=0x800000
+    
+    ldr r4,[r0]
+    mov r5,r4
+    and r4,r4,r6
+    
+    and r5,r5,r7
+    cmp r5,0
+    bne setbit23_1
+    beq setbit23_0
+    
+    setbit23_1:
+    cmp r5,r7
+    cmpeq r4,0
+    beq this_is_inf_case
+    orr r4,r4,r8
+    str r4,[r1]
+    b done_for_sign
+    
+    setbit23_0:
+    str r4,[r1]
+    b done_for_sign
+    
+    this_is_inf_case:
+    mov r4,0
+    str r4,[r1]
+    
+    done_for_sign:
+    pop {r4-r11,LR}
+    mov pc,lr
     /* YOUR getMantissa CODE ABOVE THIS LINE! Don't forget to push and pop! */
    
 
@@ -168,8 +263,218 @@ asmFmax:
      */
 
     /* YOUR asmFmax CODE BELOW THIS LINE! VVVVVVVVVVVVVVVVVVVVV  */
+    push {r4-r11,LR}
+    mov r10,0
+    BL initVariables 
+    /*
+    ldr r11,=f1
+    ldr r9,=0x7fc00000
+    str r9,[r11]
+    
+    ldr r11,=f2
+    ldr r9,=0x3f800000
+    str r9,[r11]
+    */
+    
+    ldr r0,=f1
+    ldr r1,=sb1
+    BL getSignBit
+    ldr r5,[r1]
+    
+    ldr r0,=f2
+    ldr r1,=sb2
+    BL getSignBit
+    ldr r6,[r1]
+    
+    check_Nan_Or_Inf:
+    ldr r0,=f1
+    ldr r1,=biasedExp1
+    ldr r2,=exp1
+    BL getExponent
+    ldr r7,[r1]
+    cmp r7,0xFF
+    beq f1_Nan_or_Inf
+    bne check_f2_Nan_or_Inf
+    
+    check_f2_Nan_or_Inf:
+    ldr r0,=f2
+    ldr r1,=biasedExp2
+    ldr r2,=exp2
+    BL getExponent
+    ldr r7,[r1]
+    cmp r7,0xFF
+    beq f2_Nan_or_Inf
+    bne Not_Nan_or_Inf_Case
+    
+    f1_Nan_or_Inf:
+    ldr r0,=f1
+    ldr r1,=mant1
+    BL getMantissa
+    ldr r8,[r1]
+    cmp r8,0x00400000
+    beq Nan_Case
+    bne f1_Inf_Case
+    
+    f2_Nan_or_Inf:
+    ldr r0,=f2
+    ldr r1,=mant2
+    BL getMantissa
+    ldr r8,[r1]
+    cmp r8,0x00400000
+    beq Nan_Case
+    bne f2_Inf_Case
+    
+    Nan_Case:
+    ldr r7,=fMax
+    mov r8,0x7FFFFFFF
+    str r8,[r7]
+    b Max_Var_Case
+    
+    f1_Inf_Case:
+    ldr r0,=f1
+    ldr r1,=sb1
+    BL getSignBit
+    ldr r5,[r1]
+    cmp r5,1
+    beq f2_Max
+    bne f1_Max
+    
+    f2_Inf_Case:
+    ldr r0,=f2
+    ldr r1,=sb2
+    BL getSignBit
+    ldr r5,[r1]
+    cmp r5,1
+    beq f1_Max
+    bne f2_Max
+    
+    Not_Nan_or_Inf_Case:
+    cmp r5,0
+    bne f1_neg
+    beq f1_pos
+    
+    f1_neg:
+    cmp r6,0
+    bne neg_equal_case
+    beq f2_Max
+    
+    f1_pos:
+    cmp r6,0
+    beq pos_equal_case
+    bne f1_Max
     
     
+    pos_equal_case:
+    ldr r0,=f1
+    ldr r1,=biasedExp1
+    ldr r2,=exp1
+    BL getExponent
+    ldr r8,[r1]
+    
+    ldr r0,=f2
+    ldr r1,=biasedExp2
+    ldr r2,=exp2
+    BL getExponent
+    ldr r9,[r1]
+    cmp r8,r9
+    beq pos_exp_same_case
+    bhi f1_Max
+    bls f2_Max
+    
+    pos_exp_same_case:
+    ldr r0,=f1
+    ldr r1,=mant1
+    BL getMantissa
+    ldr r8,[r1]
+    ldr r0,=f2
+    ldr r1,=mant2
+    BL getMantissa
+    ldr r9,[r1]
+    
+    cmp r8,r9
+    bhi f1_Max
+    bls f2_Max
+    
+    neg_equal_case:
+    ldr r0,=f1
+    ldr r1,=biasedExp1
+    ldr r2,=exp1
+    BL getExponent
+    ldr r8,[r1]
+    
+    ldr r0,=f2
+    ldr r1,=biasedExp2
+    ldr r2,=exp2
+    BL getExponent
+    ldr r9,[r1]
+    cmp r8,r9
+    beq neg_exp_same_case
+    bhi f2_Max
+    bls f1_Max
+    
+    neg_exp_same_case:
+    ldr r0,=f1
+    ldr r1,=mant1
+    BL getMantissa
+    ldr r8,[r1]
+    ldr r0,=f2
+    ldr r1,=mant2
+    BL getMantissa
+    ldr r9,[r1]
+    cmp r8,r9
+    bhi f2_Max
+    bls f1_Max
+    
+   
+    f1_Max:
+    ldr r7,=fMax
+    ldr r8,=f1
+    ldr r8,[r8]
+    str r8,[r7]
+    b Max_Var_Case
+    
+    f2_Max:
+    ldr r7,=fMax
+    ldr r8,=f2
+    ldr r8,[r8]
+    str r8,[r7]
+    b Max_Var_Case
+    
+    Max_Var_Case:
+    ldr r0,=fMax
+    ldr r1,=signBitMax
+    BL getSignBit
+    
+    ldr r0,=fMax
+    ldr r1,=biasedExpMax
+    ldr r2,=expMax
+    BL getExponent
+    
+    ldr r8,[r1]
+    cmp r8,0
+    beq BEM_0_case
+    b   BEM_not_0_case
+    BEM_0_case:
+    ldr r7,=expMax
+    mov r8,-126
+    str r8,[r7]
+    b BEM_case_done
+    
+    BEM_not_0_case:
+    ldr r8,[r2]
+    ldr r7,=expMax
+    str r8,[r7]
+    
+    BEM_case_done:
+    ldr r0,=fMax
+    ldr r1,=mantMax
+    BL getMantissa
+    
+    done:
+    ldr r7,=fMax
+    mov r0,r7
+    pop {r4-r11,LR}
+    mov pc,lr
     /* YOUR asmFmax CODE ABOVE THIS LINE! ^^^^^^^^^^^^^^^^^^^^^  */
 
    
